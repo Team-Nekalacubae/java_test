66d74b6edb1d34e2b46213633431e4691db2de2e
sunskwon <sunskwon@gmail.com>
2024-02-14 23:38:46 +0900

    Commit

---
+++ src/main/java/com/ohgiraffers/kss/run/Application.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.kss.run;
+
+import com.ohgiraffers.kss.view.BookMenu;
+
+public class Application {
+    public static void main(String[] args) {
+
+        BookMenu bookMenu = new BookMenu();
+        bookMenu.menu();
+    }
+}
